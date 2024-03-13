c8603d8b45572c18aecf91f1e7f08e5a65baaf9e
승엽 <tmdduqflfl@naver.com>
2024-03-13 10:00:00 +0900

    study

---
+++ src/k10/Sol4_4.java
@@ -0,0 +1,17 @@
+package k10;
+
+public class Sol4_4 {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1; i<=9; i++){
+            for(j=1; j<=9; j++)
+                System.out.printf("%d * %d = %2d",i,j,i*j);
+            System.out.println();
+
+
+
+        }
+
+
+    }
+}
