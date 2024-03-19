fed43c1da19fe32a3c3d82dc43644853d41a4b62
승엽 <tmdduqflfl@naver.com>
2024-03-19 10:00:00 +0900

    study

---
+++ src/LAST2/Sol11.java
@@ -0,0 +1,14 @@
+package LAST2;
+
+class A9 {
+    void msg() {
+        System.out.println("try");
+    }
+}
+
+public class Sol11 extends A9 {
+    public static void main(String[] args) {
+        Sol11 a = new Sol11();
+        a.msg();
+    }
+}
