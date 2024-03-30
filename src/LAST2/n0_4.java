2be3dfdbb0af9702567a82665215599a2caf3c3c
승엽 <tmdduqflfl@naver.com>
2024-03-30 10:00:00 +0900

    study

---
+++ src/LAST2/n0_4.java
@@ -0,0 +1,14 @@
+package LAST2;
+
+public class n0_4 {
+    public static void main(String[] args) {
+        String g[][] ={{"bo","bb","bc"},{"jo","sa","re"}{"mi","el","an"}};
+        for(int i=0; i<3; i++){
+            for(int j = 0; j<3;j++){
+                if(g[i][j].length()==2);
+
+                System.out.println(g[i][j]+"");
+            }
+        }
+    }
+}
