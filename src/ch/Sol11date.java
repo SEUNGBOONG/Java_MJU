0a8a4afa64cd87cca39291aa4d759cdc54c943bd
승엽 <tmdduqflfl@naver.com>
2024-04-16 10:00:00 +0900

    study

---
+++ src/ch/Sol11date.java
@@ -0,0 +1,11 @@
+package ch;
+
+import java.util.Date;
+
+public class Sol11date {
+    public static void main(String[] args) {
+        Date open=new Date();
+        System.out.println(open);
+        System.out.println(open.getTime());
+    }
+}
